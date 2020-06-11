45ba0ea9312790065eb187896727eca3f4d57e36
Jonny Chang <58116035+jonny6015@users.noreply.github.com>
2020-06-12 00:01:05 +0800

    jonny 更新于 2020年 6月12日 星期五 00时00分58秒 CST.

---
+++ lmsite-deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/env zsh
+REPO_NAME="lmsite-bookmarks"
+dateTime="$(date '+%Y%m%d_%H%M%S')"
+echo "进入项目目录："
+cd "/src/lmsite-bookmarks" || return
+git pull origin master:master
+git reset --hard origin/master
+git stash clear
+git clean  -d  -fx .
+echo "已成功拉取最新代码！"
+
+##############################################################
+#########  这里编写其余逻辑,如编译/部署等操作  #############
+##############################################################
+# 生成静态文件
+cd /src/lmsite-bookmarks
+npm install
+npm run build
+# 进入生成的文件夹,创建.nojekyll 防止Github Pages build错误
+cd /src/lmsite-bookmarks/docs/.vuepress/dist
+touch .nojekyll
+# 部署
+rm -r -f /webroot/lmsite.cn
+cp -r -f /src/lmsite-bookmarks/docs/.vuepress/dist /webroot/lmsite.cn
+echo "部署完成"
+exit 0;
+
+##############################################################
+##############################################################
+
+echo "ΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩ"
+echo "ΩΩΩΩ      已成功部署 lmsite-bookmarks!      ΩΩΩΩ"
+echo "ΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩ"
+exit 0;
